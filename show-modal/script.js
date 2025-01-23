aa8db702487c6666b9cdc0a117b573d29bcc2b76
faizanalibaig <riazfiaz6181@gmail.com>
2025-01-23 16:14:12 +0500

    complete

--- show-modal/script.js
+++ show-modal/script.js
@@ -1,1 +1,36 @@
 "use strict"
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-modal");
+const openBtn = document.querySelectorAll(".show-modal");
+
+function showModal(){
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal(){
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+openBtn.forEach(btn => {
+  btn.addEventListener("click", function(){
+    showModal();
+  })
+})
+
+closeBtn.addEventListener("click", function(){
+  closeModal();
+})
+
+overlay.addEventListener("click", function(){
+  closeModal();
+})
+
+document.addEventListener("keydown", function(e){
+  if(e.key === "Escape" && !modal.classList.contains("hidden")){
+     closeModal();
+  }
+})
