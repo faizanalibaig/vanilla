56a11142502d70f36886bf220609994cb942cc69
faizanalibaig <riazfiaz6181@gmail.com>
2025-02-06 21:45:56 +0500

    navbar functionality implementing

--- multi-random/script.js
+++ multi-random/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const extraSection = document.querySelector('.extra');
+
+window.addEventListener('scroll', () => {
+  const extraSectionDetails = extraSection.getBoundingClientRect();
+  console.log(scrollY);
+  console.log(extraSectionDetails.top);
+  if (scrollY > extraSectionDetails.top) {
+    console.log('entered');
+    if (!header.classList.contains('main-header')) {
+      header.classList.add('main-header');
+    }
+  }
+});
