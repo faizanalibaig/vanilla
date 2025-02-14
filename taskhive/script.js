78c4de9d9c2a2748c8dfc8eceb5d4fb794e5bab0
faizanalibaig <riazfiaz6181@gmail.com>
2025-02-14 22:49:52 +0500

    Form setup

--- taskhive/script.js
+++ taskhive/script.js
@@ -4,17 +4,23 @@ const header = document.querySelector('.header-section');
 const offer = document.querySelector('.what-we-offer');
 const offerRect = offer.getBoundingClientRect();
 
-console.log('event-1', offerRect.y);
+// console.log('event-1', offerRect.y);
 
 window.addEventListener('scroll', () => {
-  console.log('event', scrollY);
-  console.log('event-1', offerRect.top);
+  // console.log('event', scrollY);
+  // console.log('event-1', offerRect.top);
 
   if (scrollY > offerRect.y) {
-    console.log('entered');
+    // console.log('entered');
     header.classList.add('header');
   } else {
-    console.log('exited');
+    // console.log('exited');
     header.classList.remove('header');
   }
+});
+
+const taskInputBtn = document.querySelector('.create-task-btn');
+taskInputBtn.addEventListener('click', () => {
+  const taskInput = document.querySelector('.task-input');
+  console.log('task-input: ', taskInput.value);
 });
