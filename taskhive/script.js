f69fe187a8ac190b8c9ad96ef1148dbdc030f0e8
faizanalibaig <riazfiaz6181@gmail.com>
2025-02-17 18:51:33 +0500

    Add Task - Functionality

--- taskhive/script.js
+++ taskhive/script.js
@@ -22,5 +22,27 @@ window.addEventListener('scroll', () => {
 const taskInputBtn = document.querySelector('.create-task-btn');
 taskInputBtn.addEventListener('click', () => {
   const taskInput = document.querySelector('.task-input');
-  console.log('task-input: ', taskInput.value);
+  const taskTitle = document.querySelector('.task-title');
+  const box = document.querySelector('.block-of-offers');
+
+  const boxChild = document.createElement('div');
+  boxChild.classList.add('box');
+
+  const title = document.createElement('h1');
+  title.textContent = taskTitle.value;
+  title.classList.add('task-heading');
+  boxChild.appendChild(title);
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = taskInput.value;
+  paragraph.classList.add('task-paragraph');
+  boxChild.appendChild(paragraph);
+
+  box.appendChild(boxChild);
+
+  taskInput.value = '';
+  taskTitle.value = '';
+
+  console.log('task-input: ', taskInput.value, taskTitle.value);
+  console.log('Box: ', box.children);
 });
