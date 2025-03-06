ba77177a63aecdf82f099c5cdb17276f6d796923
faizanalibaig <riazfiaz6181@gmail.com>
2025-03-06 23:09:29 +0500

    XML Implementation

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+const request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "https://api.open-meteo.com/v1/forecast?latitude=33.6844&longitude=73.0479&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Asia/Karachi"
+);
+request.send();
+
+request.addEventListener("load", () => {
+  console.log("Data: ", JSON.parse(request.responseText));
+});
